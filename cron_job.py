fa6ea25ed8174d3457fed0bc42e72a9b9b6ac620
Swapnil Tambe <107341615+Swapnidtambe@users.noreply.github.com>
2023-06-22 01:53:56 +0530

    updated date in cron job

--- cron_job.py
+++ cron_job.py
@@ -3,8 +3,11 @@ import pytz
 from sql_connection import get_sql_connection
 connection = get_sql_connection()
 from datetime import datetime
+from datetime import date
+
 
 def store_data():
+    current_date = date.today()
     from open_ai import predict_trend
     indian_timezone = pytz.timezone('Asia/Kolkata')
     indian_time = datetime.now(indian_timezone).time()
@@ -17,8 +20,8 @@ def store_data():
     else:
         signal = 'No Trade'
     cursor = connection.cursor(buffered=True)
-    query = "INSERT INTO summary (summary, time, currency, `signal`) VALUES (%s, %s, 'GOLD',%s)"
-    cursor.execute(query, (text_data, indian_time,signal))
+    query = "INSERT INTO summary (summary, time, currency, `signal`, date) VALUES (%s, %s, 'GOLD',%s,%s)"
+    cursor.execute(query, (text_data, indian_time,signal,current_date))
     connection.commit()
 
 scheduler = BackgroundScheduler()
